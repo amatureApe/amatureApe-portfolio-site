61ba647996632f11842b291c23bfa66e5df84abf
Darian Taban <amatureApe@gmail.com>
2022-11-08 18:17:55 -0800

    add works page with dummy data

---
+++ pages/works.js
@@ -0,0 +1,24 @@
+import { Container, Box, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-time'
+
+import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
+
+const Works = () => {
+    return (
+        <Container>
+            <Heading as="h3" fontSize={20} mb={4}>
+                Works
+            </Heading>
+            <SimpleGrid columns={[1, 1, 2]} gap={6}>
+                <Section>
+                    <WorkGridItem id="inkdrop" title="Inkdrop" thumbnail={thumbInkdrop}>
+                        A markdown note-taking app
+                    </WorkGridItem>
+                </Section>
+            </SimpleGrid>
+        </Container>
+    )
+}
+
+export default Works
