85abfad256374cff82b381100010ce5ddc25e9e3
Darian Taban <amatureApe@gmail.com>
2022-11-09 19:24:01 -0800

    add more dummy data and make style fixes

--- components/navbar.js
+++ components/navbar.js
@@ -17,15 +17,17 @@ import {
 import { HamburgerIcon } from '@chakra-ui/icons'
 import ThemeToggleButton from './theme-toggle-button'
 
-const LinkItem = ({ href, path, children }) => {
+const LinkItem = ({ href, path, target, children, ...props }) => {
   const active = path === href
   const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
   return (
-    <NextLink href={href}>
+    <NextLink href={href} passHref scroll={false}>
       <Link
         p={2}
-        bg={active ? 'glassTeal' : undefined}
+        bg={active ? '#81e6d9' : undefined}
         color={active ? '#202023' : inactiveColor}
+        target={target}
+        {...props}
       >
         {children}
       </Link>
